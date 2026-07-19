73b22d5df0d637b02f7cb1c004d7ee13df52d12c
gokulgowtham0705 <gokulgowthamzzz@gmail.com>
2026-07-19 11:05:21 +0530

    Checkbox completion

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -24,19 +24,49 @@ test('Table', async ({ page }) => {
 
   await  page.locator("//span[@class='text' and contains(text(), 'Book Store')]").first().click();
  
-const targetRow = page.locator('table tr', {
-  has: page.locator('td').filter({ hasText: /^Learning/ })
+const targetRow = page.locator('table tr', {  
+has: page.locator('td').filter({ hasText: /^Learning/ })
 });
 
 
 await targetRow.locator('td:nth-child(2)').click();
 
-
 await expect(page.getByRole('button', { name: 'Back To Book Store' })).toBeVisible();
 
 await page.screenshot({ path: 'tests/screenshots/book-details.png' });
-  
- 
+
 });
 
 
+test('Text Box', async ({ page }) => {
+
+  await page.goto('https://demoqa.com/');  
+  await page.getByText('Elements').click();
+  await page.getByPlaceholder('Full Name').fill('John Doe');
+  await page.getByPlaceholder('name@example.com').fill('john@example.com');
+  await page.getByPlaceholder('Current Address').fill('123 Main St');
+  await page.locator('//textarea[@id="permanentAddress"]').fill('456 Elm St');
+  await page.getByRole('button', { name: 'Submit' }).click();
+ 
+
+  const nameText = await page.getByRole('paragraph', { name: 'name' }).textContent();
+  console.log(`Retrieved name: ${nameText}`);
+
+});
+
+
+test('CheckBox', async ({ page }) => {
+  await page.goto('https://demoqa.com/');  
+  await page.getByText('Elements').click();
+  await page.getByText('Check Box').click();
+
+  await page.locator("//span[@aria-label='Select Home']").check();
+
+  await page.locator(".rc-tree-switcher_close").click();
+  await expect(page.getByLabel('Select Desktop')).toBeChecked();
+  
+  const actualResult = await page.locator("//div[@id='result']").textContent();
+  const ExpectedResult = 'You have selected :homedesktopdocumentsdownloadsnotescommandsworkspaceofficewordFileexcelFilereactangularveupublicprivateclassifiedgeneral';
+  await expect(actualResult).toBe(ExpectedResult);
+  
+})
